8cf02713650f972b04a0c548830db99e3adcf0aa
Reaksa009 <satevada111@@gmail.com>
2026-07-03 10:03:11 +0700

    Add robust environment detection for database models

--- app/Models/DatabaseAuthenticatable.php
+++ app/Models/DatabaseAuthenticatable.php
@@ -2,7 +2,22 @@
 
 namespace App\Models;
 
-if (class_exists(\MongoDB\Laravel\Auth\User::class)) {
+$isMongo = false;
+$conn = $_ENV['DB_CONNECTION'] ?? $_SERVER['DB_CONNECTION'] ?? getenv('DB_CONNECTION');
+
+if ($conn === 'mongodb') {
+    $isMongo = true;
+} elseif (function_exists('env')) {
+    try {
+        if (env('DB_CONNECTION') === 'mongodb') {
+            $isMongo = true;
+        }
+    } catch (\Throwable $e) {
+        // Ignore errors during early load
+    }
+}
+
+if ($isMongo && class_exists(\MongoDB\Laravel\Auth\User::class)) {
     abstract class DatabaseAuthenticatable extends \MongoDB\Laravel\Auth\User
     {
     }
